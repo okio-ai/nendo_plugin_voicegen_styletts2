b2e8d462565e5b7cc56dc49925d08e7fed2d3694
faradox <felix@okio.ai>
2024-02-29 16:46:49 +0100

    chore: prepare release 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="nendo-plugin-voicegen-styletts2",
-        version="0.1.0",
+        version="0.1.1",
         description="A text to speech plugin based on StyleTTS2.",
         author="Aaron Abebe <aaron@okio.ai>",
     )
